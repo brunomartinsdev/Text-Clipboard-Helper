4d70025809875e8e80091cd8c47ae0a3d19c08a3
brunolmartins <bruno@bdevapps.com>
2015-08-05 17:52:06 -0300

    Initial Commit

---
+++ Text Clipboard Helper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Text Clipboard Helper
+//
+//  Created by Bruno Lima Martins on 8/5/15.
+//  Copyright (c) 2015 Bruno Lima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
